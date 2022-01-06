9ab5668673f948fea2c2c5125ccd4ef83e4cdeb0
4ndrei22 <72560776+4ndrei22@users.noreply.github.com>
2022-01-06 14:20:48 +0800

    Update getData.php

--- Super Admin/getData.php
+++ Super Admin/getData.php
@@ -1,21 +1,28 @@
 <?php
  include 'connect.php';
-        if(!empty($_POST)) {
-          $user = $_POST['usernameR'];
-          $Fname = $_POST['firstname'];
-          $Lname = $_POST['lastname'];
-          $ContactNum = $_POST['contact_number'];
-          $email = $_POST['email'];
-          $adminKey = "Staff";
+ if(!empty($_POST)) {
+        $user = $_POST['usernameR'];
+        $Fname = $_POST['firstname'];
+        $Lname = $_POST['lastname'];
+        $ContactNum = $_POST['contact_number'];
+        $email = $_POST['email'];
+        //$adminKey = "Staff";
+        $accessLvl = $POST['accessLvl'];
+        if ($accessLvl == 0)
+                $accessLvl= "Staff";
+        else 
+                $accessLvl= "Super Admin";
+        
+        
           $password = password_hash($_POST["passwordR"], PASSWORD_DEFAULT);
           $sql = "INSERT INTO accountcreation (adminKey, Firstname, Lastname, Email, Contactnum, Username, Password)
-                  VALUES ('$adminKey','$Fname','$Lname','$email','$ContactNum','$user','$password')";
-          $sql2 = "INSERT INTO staff_tickets (staff_name,accessLVL) VALUES ('$Fname','$adminKey')"  ;
+                  VALUES ('$accessLvl','$Fname','$Lname','$email','$ContactNum','$user','$password')";
+          $sql2 = "INSERT INTO staff_tickets (staff_name,accessLVL) VALUES ('$Fname','$accessLvl')"  ;
           
           mysqli_query($con,$sql);
           mysqli_query($con,$sql2);
-          
-          header('refresh: 1; url = AdminCreation.php');
+          echo "<meta http-equiv='refresh' content='0'>";
+          header('Location: AdminCreation.php');
             
         
         
